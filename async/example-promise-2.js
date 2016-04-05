85faaef5e9980aa2caed50d92963d7be5bda3984
mjdude <mo@syncspot.net>
2016-04-05 18:37:06 +0100

    advenced promises example

---
+++ async/example-promise-2.js
@@ -0,0 +1,21 @@
+function doWork(shouldFail){
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      if (typeof shouldFail === 'boolean' && shouldFail === true ) {
+        reject('error message');
+      }else {
+        resolve('success');
+      }
+
+    }, 1000);
+  });
+}
+
+doWork().then(function(message){
+  console.log(message);
+  return doWork(true);
+}).then(function(message){
+  console.log(message);
+}).catch(function(error){
+  console.log(error);
+});
