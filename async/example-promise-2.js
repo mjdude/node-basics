906ef64ebbb32a1a9e7ec1f1310dc4225e0967d9
mjdude <mo@syncspot.net>
2016-04-05 19:02:29 +0100

    promises with weather and location hardcoded

--- async/example-promise-2.js
+++ async/example-promise-2.js
@@ -1,21 +1,38 @@
-function doWork(shouldFail){
+// function doWork(shouldFail){
+//   return new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//       if (typeof shouldFail === 'boolean' && shouldFail === true ) {
+//         reject('error message');
+//       }else {
+//         resolve('success');
+//       }
+//
+//     }, 1000);
+//   });
+// }
+//
+// doWork().then(function(message){
+//   console.log(message);
+//   return doWork(true);
+// }).then(function(message){
+//   console.log(message);
+// }).catch(function(error){
+//   console.log(error);
+// });
+function getLocation(){
   return new Promise(function(resolve, reject){
-    setTimeout(function(){
-      if (typeof shouldFail === 'boolean' && shouldFail === true ) {
-        reject('error message');
-      }else {
-        resolve('success');
-      }
-
-    }, 1000);
+    resolve('London');
   });
 }
 
-doWork().then(function(message){
-  console.log(message);
-  return doWork(true);
+function getWeather(location){
+  return new Promise(function(resolve, reject){
+    resolve('its 78 in ' + location);
+  });
+}
+
+getLocation().then(function(location){
+  return getWeather(location);
 }).then(function(message){
   console.log(message);
-}).catch(function(error){
-  console.log(error);
 });
