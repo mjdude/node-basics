5c4fb88ff3457c1b1f4826d39201854c3d417d6f
mjdude <mo@syncspot.net>
2016-04-06 14:18:24 +0100

    added index html

--- web-server/server.js
+++ web-server/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res){
-  res.send('Hello Express');
-});
+var port = 3000;
 
 app.get('/about', function(req, res){
     res.send('About Us');
 });
 
-app.listen(3000);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function() {
+  console.log('express server started on port ' + port);
+
+});
