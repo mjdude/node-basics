6f63698efda9b23c282f37b96aa694a2084d7537
mjdude <mo@syncspot.net>
2016-04-06 15:32:00 +0100

    added middleware to server

--- web-server/server.js
+++ web-server/server.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var PORT = 3000;
+var middleware = {
+  requireAuthentication: function(req, res, next){
+      console.log('private route hit');
+      next();
+  },
+  logger: function(req, res, next){
+    var date = new Date().toString();
+    console.log('Request ' + date + ' : ' + req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
 
-app.get('/about', function(req, res){
+app.use(middleware.logger);
+app.get('/about',middleware.requireAuthentication ,function(req, res){
     res.send('About Us');
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function() {
-  console.log('express server started on port ' + port);
+app.listen(PORT, function() {
+  console.log('express server started on port ' + PORT);
 
 });
